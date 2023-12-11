5e10514e11eccd6cd7ae36902f75fdbc198a5419
JDanielAcuña <80725943+JoXerph@users.noreply.github.com>
2023-12-11 18:43:13 -0500

    Invoke y pausa basicos

--- ProyectoGrupo3/Assets/InvokeScript.cs
+++ ProyectoGrupo3/Assets/InvokeScript.cs
@@ -4,22 +4,41 @@ using UnityEngine;
 
 public class InvokeScript : MonoBehaviour
 {
-
+    [SerializeField] private GameObject Activo;
     [SerializeField] private GameObject stick;
     // Start is called before the first frame update
     void Start()
     {
-        Invoke(nameof(Spawn), 2);
+        InvokeRepeating(nameof(Spawn), 2, 4);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Pausa o Continuar
+        if (Input.GetKeyDown((KeyCode)'p'))
+        {
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;//Pausar el tiempo
+            }
+            else if (Time.timeScale == 0)
+            {
+                Time.timeScale = 1;//Continuar el tiempo
+            }
+        }
+
     }
 
     private void Spawn()
     {
-        Instantiate(stick, new Vector3(0,1,0), Quaternion.identity);
+        float x = Random.Range(-4.0f,16.0f);
+        float z = Random.Range(-4.0f,16.0f);
+        Instantiate(stick, new Vector3(x,1,z), Quaternion.identity);
+    }
+
+    private void Active(GameObject obj)
+    {
+
     }
 }
