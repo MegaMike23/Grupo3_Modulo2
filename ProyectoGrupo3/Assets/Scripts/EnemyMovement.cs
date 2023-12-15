4f501fb3a73ccf3189757a4f2a58ab5fcd8b61ac
JDanielAcuña <80725943+JoXerph@users.noreply.github.com>
2023-12-14 21:33:51 -0500

    Movimiento enemigo final

--- ProyectoGrupo3/Assets/Scripts/EnemyMovement.cs
+++ ProyectoGrupo3/Assets/Scripts/EnemyMovement.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TreeEditor;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    public Transform targetObj;//Ninja
     public float rangoAlerta;//Rango de detección
     public LayerMask capaJugador;//Capa que detecta al jugador
+    public Transform jugador;//Posición del jugador
+    public float velocidad;//Velocidad de movimiento del enemigo
     bool estarAlerta;//Detecta si el jugador entró en el rango de detección
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,15 +20,27 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+      
         //Detectar la presencia del jugador
         estarAlerta = Physics.CheckSphere(transform.position, rangoAlerta, capaJugador);
 
-        Vector3 newPosition = new Vector3((targetObj.position.x)+1.0f, targetObj.position.y, (targetObj.position.z)+1.0f);
-        transform.position = Vector3.MoveTowards(this.transform.position, newPosition, 5 * Time.deltaTime);
+        //Si detecta al jugador
+        if (estarAlerta)
+        {
+            //Mirar al jugador
+            transform.LookAt(new Vector3(jugador.position.x,transform.position.y,jugador.position.z));
+            //Que se mueva hacia el jugador
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(jugador.position.x, transform.position.y, jugador.position.z), velocidad * Time.deltaTime);
+        }
+
+        /*Vector3 newPosition = new Vector3((targetObj.position.x)+1.0f, targetObj.position.y, (targetObj.position.z)+1.0f);
+        transform.position = Vector3.MoveTowards(this.transform.position, newPosition, 5 * Time.deltaTime);*/
     }
 
-    private void onDrawGizmos()
+    //Dibujar rango esfera para medir radio de detección en modo dev
+    private void OnDrawGizmos()
     {
-
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, rangoAlerta);
     }
 }
