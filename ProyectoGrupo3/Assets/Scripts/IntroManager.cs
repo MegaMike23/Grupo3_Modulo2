c179bcbe77f7a80f522f24702a5d21f5a1d99b10
MegaMike23 <47559570+MegaMike23@users.noreply.github.com>
2023-12-18 11:32:51 +0100

    Trabajando en INTRO

---
+++ ProyectoGrupo3/Assets/Scripts/IntroManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroManager : MonoBehaviour
+{
+
+    void Start()
+    {
+        AudioManager.Instance.PlayMusic("Intro");
+    }
+
+
+}
